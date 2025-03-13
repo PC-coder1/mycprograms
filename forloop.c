5943eb5dcfe93d774ebee6f98a8b5b891683fa35
Prithviraj Chavan <cprithviraj713@gmail.com>
2025-03-13 06:42:34 +0530

    Add files via upload

---
+++ forloop.c
@@ -0,0 +1,20 @@
+//loop= used to  repeateadly execute block of statements
+// forloop=if we know exact no of statements
+
+// for loop (initialize: condition: increment or decrement)
+// {statements
+// will execute till our condition becomes
+
+//eg print the goodmorning for 5 times
+#include <stdio.h>
+
+int main ()
+{
+    int i;
+    for (i=1;i<5; i++) 
+    {
+        printf("good morning\n");
+    }
+    return 0;
+
+}
