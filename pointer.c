0f9a0c57bb483d783400a8f376043b4238a22ebf
Prithviraj Chavan <cprithviraj713@gmail.com>
2025-03-14 22:59:57 +0530

    Add files via upload

---
+++ pointer.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*here we i have demonstrated pointer.instead of writing x multiple times
+we use pointer to refer to the same memory location. we can change value of x
+by updatint *ptr, avoiding redundant variable declarations.
+ */
+
+
+int main() {
+
+
+int x=20;
+int *ptr = &x; // *ptr= it is pointer that stores address of x. so we are telling compiler to store address of int x.
+printf("\n%d", *ptr);  //*ptr means access the value of x
+printf("\n%d", ptr); // ptr means access the address of x
+
+printf("\n%d", x); /// x=20
+printf("\n%d", &x); ///address of x
+*ptr= 40; ///  now we updated value of x=20 and gave new value x=40 with help of *ptr
+printf("\n%d", *ptr);  ///*ptr=40
+
+///printf("%d", &x);
+//printf("%d", *ptr);
+//printf("%d", ptr);
+//printf("\n%d", x);
+//printf("\n%d", &x);
+
+}
